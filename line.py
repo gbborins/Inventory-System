b8712d39675b09b26d312d82e3179317fc69a12b
Gbborins <105250103+gbborins@users.noreply.github.com>
2026-01-31 19:59:31 -0300

    Line fixed

--- line.py
+++ line.py
@@ -1,18 +1,20 @@
-last = int(input(f"quantidade inicial"))
-line = list(range(1,last+1))
 while True:
+    try:
+        last = int(input(f"Quantidade inicial de clientes: "))
+    except (ValueError, TypeError) as er:
+        print("Por favor digite um número inteiro")
+        continue
+    line = list(range(1,last+1))
+    break
+while True:
+    ultimo = line[-1] if len(line) > 0 else 0
     print(f"{len(line)} clientes restantes")
     print(f"fila atual: {line}")
     choice = str(input("Digite C(adicionar cliente), A(atendimento), F(fechar)"))
     if choice.lower() == "a":
-        if len(line) > 0:
-            aten = line.pop(0)
-            print(f"Cliente {aten} atendido"}
-        else:
-            print(f"Não há ninguém")
+        print(f"Cliente {line.pop(0)} atendido") if len(line) > 0 else print(f"Não há ninguém")
     elif choice.lower() == "c":
-        ultimo += 1
-        fila.append(ultimo)
+        line.append(ultimo + 1)
     elif choice.lower() == "f":
         break
     else:
